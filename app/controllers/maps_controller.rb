bf683504ae2d5077e607dad96d42eb97eb5a90fc
Zhao Li <zhaol@hawaii.edu>
2013-12-09 04:45:28 -0500

    migrate from wget to Ruby open

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -15,10 +15,9 @@ class MapsController < ApplicationController
   
   def get_coordinates
     if coordinates_file = params[:coordinates_file]
-      @coordinates = coordinates_file.read.html_safe
+      @coordinates = coordinates_file.read
     else
-      coordinates_file = `wget https://raw.github.com/zhaol/ee160_app/master/public/uhm.txt -O -`
-      @coordinates = coordinates_file.html_safe
+      @coordinates = open('https://raw.github.com/zhaol/ee160_app/master/public/uhm.txt').read
     end
   end
 end
