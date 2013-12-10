f5afffdd0ea20e01868d0345a001ecba1bd024ba
Zhao Li <zhaol@hawaii.edu>
2013-12-10 10:37:18 -0500

    reorganize maps.js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,30 +1,25 @@
-$(function() {
-  function initialize() {
-    if ($(coordinates).length > 0) {
-      var lat = $(coordinates).first()[0].lat;
-      var long = $(coordinates).first()[0].long;
-    } else {
-      var lat = 0;
-      var long = 0;
-    }
-    var myLatlng = new google.maps.LatLng(lat, long);
-    var mapOptions = {
-      zoom: 4,
-      center: myLatlng
-    }
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    
-    if ($(coordinates).length > 0) {
-      $(coordinates).each(function() {
-        var aLatLng = new google.maps.LatLng(this.lat, this.long);
-        var marker = new google.maps.Marker({
-          position: aLatLng,
-          map: map,
-          title: this.title
-        });
-      });
-    }
+function initialize_map() {
+  if ($(coordinates).length > 0) {
+    var lat = $(coordinates).first()[0].lat;
+    var long = $(coordinates).first()[0].long;
   }
+  var myLatlng = new google.maps.LatLng(lat, long);
+  var mapOptions = {
+    zoom: 17,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   
-  google.maps.event.addDomListener(window, 'load', initialize);
+  $(coordinates).each(function() {
+    var aLatLng = new google.maps.LatLng(this.lat, this.long);
+    var marker = new google.maps.Marker({
+      position: aLatLng,
+      map: map,
+      title: this.title
+    });
+  });
+}
+  
+$(function() {
+  google.maps.event.addDomListener(window, 'load', initialize_map);
 });
